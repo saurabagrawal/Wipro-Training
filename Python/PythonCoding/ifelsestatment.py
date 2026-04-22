4689bda5bed118aae29cc3de45dea565ef6035cb
saurabagrawal <agarwalsaurabh662@gmail.com>
2026-04-22 15:14:38 +0530

    Python basics

---
+++ Python/PythonCoding/ifelsestatment.py
@@ -0,0 +1,23 @@
+# big 2
+# num1 = int(input("Enter a number"))
+# num2 = int(input("Enter the numer"))
+#
+# if num1 == num2:
+#     print("Both number are equal")
+# elif num1 > num2:
+#     print(num1, "num1 is greater")
+# else:
+#     print(num2 , "num2 is greater")
+
+#big 3
+num1 = int(input("Enter the number"))
+num2 = int(input("Enter the number"))
+num3 = int(input("Enter the number"))
+if num1 > num2 and num1> num3:
+    print(num1 , "is greater")
+elif num2 > num3 and num2 > num3:
+    print(num2 , "is greater")
+elif num3 > num1 and num3 > num2:
+    print(num3, "is greater")
+else:
+    print("All the number are equal")
